643c7e7977cf28a7f3bcacec87c98d6c91719ae3
Marek Sierbin <msierbin@itsg.com.pl>
2019-03-15 11:34:30 +0100

    Support for uploading the entire folder structure

--- UploadMojo.java
+++ UploadMojo.java
@@ -10,7 +10,7 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,55 +22,52 @@ import java.util.stream.Collectors;
 public class UploadMojo extends AbstractMojo {
 	private final Log log = getLog();
 
-	@Parameter(property = "connectionString", required = true)
-	public String connectionString;
+    @Parameter(property = "connectionString", required = true)
+    public String connectionString;
 
-	@Parameter(property = "rootDir", required = true /*TODO*/)
-	public String rootDir;
+    @Parameter(property = "rootDir", required = true /*TODO*/)
+    public String rootDir;
 
-	@Override
-	public void execute() throws MojoExecutionException {
-		try {
+    @Override
+    public void execute() throws MojoExecutionException {
+        try {
 			log.info(String.format("Using root directory of: '%s'", Paths.get(rootDir).toAbsolutePath().toString()));
 			List<Path> files = Files
 				.walk(Paths.get(rootDir))
+				.filter(Files::isRegularFile)
 				.collect(Collectors.toList());
 
 			final CloudBlobContainer container = CloudStorageAccount
 				.parse(connectionString)
 				.createCloudBlobClient()
 				.getContainerReference("$web");
-			for (Path file : files) {
-				sendFileTocloud(container, file);
-			}
-		} catch (StorageException ex) {
-			log.error(ex);
-			throw new MojoExecutionException(String.format(
+			files.forEach(f -> {
+				final String realPath = f.toString();
+				final String cloudStoragePath = f.toString().substring(rootDir.length()+1);
+				try {
+					final CloudBlockBlob blob = container.getBlockBlobReference(cloudStoragePath);
+					blob.uploadFromFile(realPath);
+					log.info(String.format("Content of '%s' send to '%s'", realPath, cloudStoragePath));
+				} catch (URISyntaxException | StorageException | IOException e) {
+					e.printStackTrace();
+				}
+			});
+
+        } catch (StorageException ex) {
+        	log.error(ex);
+            throw new MojoExecutionException(String.format(
 				"Error returned from the service. Http code: %d and error code: %s",
 				ex.getHttpStatusCode(),
 				ex.getErrorCode()
-			), ex);
-		} catch (Exception ex) {
-			log.error(ex);
+			));
+        } catch (Exception ex) {
+        	log.error(ex);
 			throw new MojoExecutionException(String.format(
 				"Error returned: %s",
 				ex.getMessage()
-			), ex);
+			));
 		}
-	}
+    }
 
-	private void sendFileTocloud(CloudBlobContainer container, Path file) throws MojoExecutionException {
-		if (!Files.isRegularFile(file)) {
-			return;
-		}
-		final String realPath = file.toString();
-		final String cloudStoragePath = file.toString().substring(rootDir.length()+1);
-		try {
-			final CloudBlockBlob blob = container.getBlockBlobReference(cloudStoragePath);
-			blob.uploadFromFile(realPath);
-			log.info(String.format("Content of '%s' send to '%s'", realPath, cloudStoragePath));
-		} catch (URISyntaxException | StorageException | IOException ex) {
-			throw new MojoExecutionException(ex.getMessage(), ex);
-		}
-	}
+
 }
